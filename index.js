eda7841def8b6f01fefb652a65f2880418f9124a
ffadilaputra <i.fadilaputra@gmail.com>
2017-09-25 14:45:49 +0700

    Scraping profile medium

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
-let scraper = require('scraper');
+var Scraper = require('./scraper.js');
 
-let scraper = new Scraper({
-      baseUrl:"https://medium.com"
+var scraper = new Scraper({
+    baseUrl: 'https://medium.com'
 });
+
+scraper.scrape('@ffadilaputra',(err,$)=>{
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    let name = $('h1.hero-title').text();
+    let desc = $('p.hero-description').text();
+
+    let following = $('div.buttonSet--profile > a').map(()=>{
+      return $(this).text();
+    })
+
+    let jajal = $('div.buttonSet--profile > a').text();
+
+    let result = {
+        name:name,
+        desc:desc,
+        following:following[0],
+        followers:following[1],
+        jajal:jajal
+    }
+
+    let json = JSON.stringify(result);
+    console.log(json);
+
+})
--- scraper.js
+++ scraper.js
@@ -1,40 +1,42 @@
-let cheerio = require('cheerio');
-let extend = require('extend');
-let request = require('request');
+var cheerio = require('cheerio');
+var extend = require('extend');
+var request = require('request');
 
-module.exports = (userOptions) => {
-
+module.exports = function(userOptions) {
   var options = extend({
-      method  : 'GET',
-      baseUrl : '',
-      gzip    : true,
-      forever : true,
-      headers : {
+    method: 'GET',
+    baseUrl: '',
+    gzip: true,
+    forever: true,
+    headers: {
       'Cache-Control': 'max-age=0',
       'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36',
-    }
-  },userOptions);
+    },
+  }, userOptions);
 
-  scrape((url,callback) => {
-     var req = getRequestInstance();
-        req(url,(error,response,body)=>{
+  function scrape(url, callback) {
+    var req = getRequestInstance();
 
-          if (error) {
-            return callback(error);
-          }
+    req(url, function(error, response, body) {
+      if (error) {
+        return callback(error);
+      }
 
-          if (response.statusCode !== 200) {
-            return callback(new Error('Failed to scrape page'))
-          }
+      if (response.statusCode !== 200) {
+        return callback(new Error('Failed to scrape the page.'));
+      }
 
-          var $ = cheerio.load(body);
-          return request.defaults(options);
+      var $ = cheerio.load(body);
 
-          return {
-              scrape : scrape,
-          };
+      return callback(null, $);
+    });
+  }
 
-        })
-  })
+  function getRequestInstance() {
+    return request.defaults(options);
+  }
 
-}
+  return {
+    scrape: scrape,
+  };
+};
